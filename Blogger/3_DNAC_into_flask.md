72521f36aab103b09b3e28f81227c8ad238e86c3
sabibby <sabibby@cisco.com>
2023-01-12 18:11:58 +0000

    Daily sync

--- Blogger/3_DNAC_into_flask.md
+++ Blogger/3_DNAC_into_flask.md
@@ -19,13 +19,15 @@
 
 Now we have a basic flask app and the DNAC APIs we need to use, we will look at combining the two together.
 
+[The full project files are here](/Blogger/DNAC_API/DNAC_Web_Server/)
+
 <br>
 
 # Arranging the folder structure
 
 To make this project portable, so that anyone can copy it and modify it for their own deployment we split the code files into two, where one will take care of accessing DNAC and getting a token, the other will hold all our API calls.
 
-Also remembering that the flask app looks in the *./tempaltes* folder for the HTML file to render, our prokect folder will look like this:
+Also remembering that the flask app looks in the *./templates* folder for the HTML file to render, our project folder will look like this:
 
 - DNAC_Web_Server (Project folder)
   - templates (templated folder)
@@ -38,42 +40,27 @@ Also remembering that the flask app looks in the *./tempaltes* folder for the HT
 
 # Setup the DNAC python files
 
-The two DNAC~.py files are created in the directory,
+The two DNAC~.py files are created in the directory:
 
-DNAC_data.py
+**DNAC_API.py** : this contains our required APIs as functions, i wont re-paste them here as they are mostly the same as our previous post. The main difference is the print statements have been removed & the functions return that data as some sort of object (list/dic] to allow us to pass it the the flask app for rendering in Jinja2)
+
+
+**DNAC_data.py** : this uses the devnet sandbox DNAC centre & resolves the IP for us.
 ```python
-"""This Uses the API to get the follwing data from functions
-Devices SNMP server location and contact info
-SPFs within each box
-finds devices based on a confiured port description
-The script is Cisco Confidential information and you should keep it securely
-You are granted limited license to modify and enhance the provided Source Code [script] solely for your internal use and only to the extent we expressly permit
-The script comes with no warranty of any kind, which means that if there are faults/errors in the script Cisco has no obligation to provide any help to resolve them
-"""
-
-__author__ = 'sabibby'
-#!/usr/bin/env python3.10
-
-import requests
-from requests.auth import HTTPBasicAuth
-import json
-import socket
-
 requests.urllib3.disable_warnings()
 DNAC_IP = socket.getaddrinfo("sandboxdnac.cisco.com", 443)[0][4][0]
 
-###Get/set the stuff we need to use
+
 def dnac_server(PASSWORD='Cisco123!',IP=DNAC_IP,PORT='443',USER='devnetuser'):
     """[summary]
     This is the DNAC info that needs updating
     user will be asked for password on script execution
     """
-    DNAC_IP = DNAC_IP
+    DNAC_IP = IP
     DNAC_PORT = PORT
     DNAC_USER = USER
     DNAC_PASSWORD = PASSWORD
     dnac_data = (DNAC_IP,DNAC_PORT,DNAC_USER,DNAC_PASSWORD)
-
     return dnac_data
 
 def get_token(dnac_system):
@@ -83,6 +70,46 @@ def get_token(dnac_system):
     url = (f'https://{dnac_system[0]}:{dnac_system[1]}/api/system/v1/auth/token')
     headers = {'content-type': 'application/json'}
     resp = requests.post(url, auth=HTTPBasicAuth(username=dnac_system[2], password=dnac_system[3]), headers=headers,verify=False)
+    ####Add in error to catch bad password 
     token = resp.json()['Token']
     return token
+```
+
+<br>
+
+
+# Setup the flask routes and web pages
+The skeleton for flask we build in the first blog post, all we need to do now is create additional functions with the @route property to call the DNAC API functions and render the results:
+
+In the Web_app.py file we need to import the DNAC files for use, as well as some other useful modules:
+```python
+ ###Import the local DNAC scripts:
+import DNAC_API
+import DNAC_data
+
+ ###Import for the files updates needed
+import os
+from datetime import date
+
+ ###Allow us to reload modules
+import importlib
+
+ #Used to set the execute in the same folder for relative paths to use
+os.chdir((os.path.dirname(os.path.abspath(__file__))))
+```
+
+
+
+The first webepage
+
+```python
+@app.route('/',methods = ['POST', 'GET'])
+def index():
+   size = 0
+   datestamp = 'Missing'
+   if request.method == 'GET':
+      if len(os.listdir('cache')) != 0:
+         size = sum([os.path.getsize(x) for x in os.scandir('cache')])//1024
+         datestamp = str(date.fromtimestamp(os.path.getmtime('cache')))
+      return render_template('dnac_setup.html', data=DNAC_data.dnac_server(), size=size, cache_timestamp=datestamp)
 ```
---
+++ Blogger/DNAC_API/DNAC_Web_Server/Web_app.py
@@ -0,0 +1,174 @@
+
+###This import needed to parse templates
+from flask import render_template
+
+###Add support for POST calls
+from flask import request
+from flask import Flask, redirect, url_for, request
+
+###Base import
+from flask import Flask
+
+###Import the local DNAC scripts:
+import DNAC_API
+import DNAC_data
+
+###Import for the files updates needed
+import os
+from datetime import date
+
+###Allow us to reload modules
+import importlib
+
+###Sets the app up
+app = Flask(__name__)
+
+
+####Need to add buttin for UDF update with SNMP...
+
+#Need to set the execute in the same folder for relative paths to use
+os.chdir((os.path.dirname(os.path.abspath(__file__))))
+
+@app.route('/',methods = ['POST', 'GET'])
+def index():
+   size = 0
+   datestamp = 'Missing'
+   if request.method == 'GET':
+      if len(os.listdir('cache')) != 0:
+         size = sum([os.path.getsize(x) for x in os.scandir('cache')])//1024
+         datestamp = str(date.fromtimestamp(os.path.getmtime('cache')))
+      return render_template('dnac_setup.html', data=DNAC_data.dnac_server(), size=size, cache_timestamp=datestamp)
+
+
+#####Current need to write
+###Purge the cache when "flush" is run
+###Purge the local cahce when the 'update dnac' is run
+###use the local cache for all if its there.. (descriptions)
+
+
+@app.route('/update',methods = ['POST', 'GET'])
+def update_system():
+   if request.method == 'POST':
+      IP = "'"+request.form.get("IPAddress")+"'"
+      PORT = "'"+request.form.get("port")+"'"
+      USER = "'"+request.form.get("Username")+"'"
+      ####Need to add a check to see if the three above varaibles have an ', if they do error messsage
+      ####And re-do
+      #
+      with open('DNAC_data.py', 'r') as input:
+         with open("temp.txt", "w") as output:
+            # iterate all lines from file
+            for line in input:
+                  # if line starts with substring 'shown below' then don't write it in temp file
+                  if not line.strip("\n").startswith('def dnac_server(PASSWORD='):
+                     output.write(line)
+                  else:
+                     output.write("def dnac_server(PASSWORD='Not Cached',IP="+IP+",PORT="+PORT+",USER="+USER+"):\n")
+      # replace file with original name
+      os.replace('temp.txt', 'DNAC_data.py')
+      importlib.reload(DNAC_data)
+      return render_template('dnac_setup.html', data=DNAC_data.dnac_server())
+   if request.method == 'GET':
+      return render_template('dnac_update.html', data=DNAC_data.dnac_server())
+
+
+@app.route('/flush',methods = ['POST', 'GET'])
+def flush_system():
+   if request.method == 'POST':
+      return 'This is an unuset toilet flush_system, post'
+   if request.method == 'GET':
+      os.chdir('cache')
+      #return os.listdir()
+      for file in os.listdir():
+         os.remove(file)
+      os.chdir("..")
+      return redirect(url_for('index'))
+      #return os.listdir()
+
+#os.chdir('cache_copy')
+#for file in os.listdir():
+#    os.remove(file)
+
+
+@app.route('/cache',methods = ['POST', 'GET'])
+def update_cache():
+   if request.method == 'POST':
+      ##Need a bunch of APIs to download the local needed cache
+      Password = request.form.get("Password")
+      dnacs = DNAC_data.dnac_server(Password)
+      token = DNAC_data.get_token(dnacs)
+      ##Get ALL deivces in DNAC
+      devices = DNAC_API.get_devices(dnacs, token, 'ALL')
+      with open('./cache/get_devices.txt', 'w+') as f:
+            f.write(str(devices))
+      ##Get ALL interfaces as the search string is empty
+      find_port = DNAC_API.find_port(dnacs, token, devices, "")
+      with open('./cache/find_port.txt', 'w+') as f:
+            f.write(str(find_port))
+      ##GET all the SFP info on ALL nodes
+      get_sfp = DNAC_API.get_sfp(dnacs, token, devices)
+      with open('./cache/get_sfp.txt', 'w+') as f:
+         for line in get_sfp:
+            f.write(str(line))
+      return 'Post :: The cache is updated'
+   if request.method == 'GET':
+      return render_template('cache.html')
+
+
+##Here we get the dnac login info to get a authC token and then search port descriptions fro strings
+@app.route('/description',methods = ['POST', 'GET'])
+def dnac_go_interface():
+   ###Need to wirtie code for the local cahce to run...
+   if request.method == 'POST':
+      """
+      if len(os.listdir('cache')) != 0:
+         with open('./cache/find_port.txr','rt') as f:
+            .readlines()
+         Password = request.form.get("Password")
+         search = request.form.get("Search")
+         dnacs = DNAC_data.dnac_server(Password)
+         token = DNAC_data.get_token(dnacs)
+         devices = DNAC_API.get_devices(dnacs, token)
+         searched_data = DNAC_API.find_port(dnacs, token, devices, search)
+         return render_template('rendered_search.html', search_str=search, found_devices=searched_data)
+      else:
+      """ #sudo usertool.pl -p 'admin C1sco123!'
+      Password = request.form.get("Password")
+      search = request.form.get("Search")
+      dnacs = DNAC_data.dnac_server(Password)
+      token = DNAC_data.get_token(dnacs)
+      devices = DNAC_API.get_devices(dnacs, token)
+      searched_data = DNAC_API.find_port(dnacs, token, devices, search)
+      return render_template('rendered_search.html', search_str=search, found_devices=searched_data)
+   if request.method == 'GET':
+      return render_template('dnac.html')
+
+
+##Here we get the dnac login info to get a authC token and then get all the SFP data
+@app.route('/sfp',methods = ['POST', 'GET'])
+def dnac_get_sfp():
+   if request.method == 'POST':
+      Password = request.form.get("Password")
+      dnacs = DNAC_data.dnac_server(Password)
+      token = DNAC_data.get_token(dnacs)
+      devices = DNAC_API.get_devices(dnacs, token)
+      searched_data = DNAC_API.get_sfp(dnacs, token, devices)
+      return render_template('rendered_sfp.html', sfps=searched_data)
+   if request.method == 'GET':
+      if len(os.listdir('cache')) != 0:
+         searched_data = []
+         with open('./cache/get_sfp.txt', 'r') as f:
+            for line in f:
+               searched_data.append(line)
+         return render_template('rendered_sfp.html', sfps=searched_data)
+      else:
+         return render_template('dnac_sfp.html')
+
+
+
+###Sets up the flask server if this code is called directly
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=81)
+
+
+
